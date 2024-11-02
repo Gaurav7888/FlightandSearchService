b5a9580caedc2c6bc13bee6cb92c510cb45d0438
gosshhh <gaurav.sarkar@intel.com>
2024-11-02 22:15:53 +0530

    Added City Repository

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {PORT} = require("./config/serverConfig");
+const { PORT } = require("./config/serverConfig");
+const CityRepository = require("./respository/city-repository");
 
 const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-    
+
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
-    })
+        const repo = new CityRepository();
+        repo.createCity({name: "Banaglore"});
+    });
 }
 
 setupAndStartServer()
